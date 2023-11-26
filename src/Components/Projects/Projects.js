909707bb5538a70aa45c3eb6c3e8c912c8e9ead9
yashwanthwebdev <yashwanthsrinivaswebdev@gmail.com>
2023-11-26 16:16:40 +0530

    resposnsive layout changes to projects after scroll snap changes 8

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -30,7 +30,7 @@ function Projects(props) {
     <Stack
       // ref={projectsRef}
       style={{
-        height: "100svh",
+        minHeight: "100svh",
         scrollSnapAlign: "end",
         scrollSnapStop: "always",
         background:
